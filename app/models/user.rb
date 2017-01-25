7682654a378d891339fbf943cc297bac21aa2cea
meiordac <matias.iordache@gmail.com>
2017-01-25 18:31:04 +0100

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
    has_secure_password
+   validates :email, uniqueness: true
+   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+   validates :name, presence: true
+
+   has_many :comments, dependent: :destroy
+   has_many :posts, dependent: :destroy
 end
